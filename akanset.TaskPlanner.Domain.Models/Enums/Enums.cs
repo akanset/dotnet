394083bd046da59ff56c79b7a2de11c9c002c61b
akanset <akanishchev11.04@gmail.com>
2025-09-17 19:54:50 +0300

    PZ1 working commit

---
+++ akanset.TaskPlanner.Domain.Models/Enums/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace akanset.TaskPlanner.Domain.Models
+{
+    public enum Priority
+    {
+        None,
+        Low,
+        Medium,
+        High,
+        Urgent,
+    }
+    public enum Complexity
+    {
+        None,
+        Minutes,
+        Hours,
+        Days,
+        Weeks,
+    }
+}
---
+++ akanset.TaskPlanner/Program.cs
@@ -0,0 +1,56 @@
+﻿using akanset.TaskPlanner.Domain.Logic;
+using akanset.TaskPlanner.Domain.Models;
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        List<WorkItem> list = new List<WorkItem>();
+
+        SimpleTaskPlanner planner = new SimpleTaskPlanner();
+        
+        
+        while (true)
+        {
+            Console.WriteLine("Choose the option: (type the number)" + '\n' + "1. Create new Task." + '\n' + "2. See list of Tasks." + '\n' + "3. Exit.");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter Title:");
+                    string title = Console.ReadLine();
+
+                    Console.WriteLine("Enter Due Date (dd.mm.yyyy):");
+                    string dueDate = Console.ReadLine();
+
+                    Console.WriteLine("Enter Priority: (0-4)");
+                    int prior = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Complexity: (0-4)");
+                    int comp = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter Description:");
+                    string desc = Console.ReadLine();
+
+                    WorkItem toAdd = new WorkItem(title, DateTime.Parse(dueDate), (Priority)prior, (Complexity)comp, desc);
+                    list.Add(toAdd);
+
+                    break;
+                case 2:
+                    WorkItem[] newItem = planner.CreatePlan(list.ToArray());
+
+                    Console.WriteLine("List:");
+
+                    foreach (var item in newItem)
+                    {
+                        Console.WriteLine(item.ToString());
+                    }
+
+                    break;
+                case 3:
+                    Environment.Exit(0);
+
+                    break;
+            }
+        }
+    }
+}
